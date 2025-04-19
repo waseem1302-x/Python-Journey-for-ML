f2d286b0aa00d391fc544a95c66e5dd740752820
Waseem Mushtaq <waseemmushtaq045@gmail.com>
2025-04-19 18:48:35 +0800

    Get hands on experience about list by doing mini tasks

--- 03_DataStructures/1_lists.py
+++ 03_DataStructures/1_lists.py
@@ -28,3 +28,72 @@ print("Revered List" , my_fav_youtubers[::-1])
 
 
 
+# Step 1
+shopping_cart = ["Milk", "Eggs", "Bread", "Oil"]
+print(shopping_cart)
+
+# Step 2
+shopping_cart.append("Butter")
+print(shopping_cart)
+
+# Step 3
+shopping_cart.insert(2, "Detergent")
+print(shopping_cart)
+
+# Step 4
+if "Oil" in shopping_cart:
+    shopping_cart.remove("Oil")
+    print(shopping_cart)
+
+# Step 5
+shopping_cart.pop()
+print(shopping_cart)
+
+# Step 6
+shopping_cart.sort()
+
+# Final Output
+print("Final Shopping Cart:", shopping_cart)
+
+
+study_sessions = [45, 60, 30, 90, 75, 50]
+print("Original: ", study_sessions)
+
+# Sort the list
+study_sessions.sort()
+print("Sorted (Ascending): ", study_sessions)
+
+# Longest and Shortest
+longest = max(study_sessions)
+shortest = min(study_sessions)
+print("Longest Session:", longest, "minutes")
+print("Shortest Session:", shortest, "minutes")
+
+# Average calculation
+average = sum(study_sessions) / len(study_sessions)
+print("Average Study Time:", round(average, 1), "minutes")
+
+# Reversed
+study_sessions.reverse()
+print("Descending Order: ", study_sessions)
+
+
+Smart_Cart = ['apple', 'bread', 'milk', 'protein bar', 'rice', 'yogurt']
+print("Orignal: ", Smart_Cart)
+
+Smart_Cart.append("Orange")
+Smart_Cart.append("Banana")
+print("2 More Items added: ", Smart_Cart)
+
+Smart_Cart[3] = "Chips"
+print("Replaced: ", Smart_Cart)
+
+if "Chips" in Smart_Cart:
+    Smart_Cart.remove("Chips")
+    print("Removed unhealthy item: ", Smart_Cart)
+
+Smart_Cart.sort()
+print("Sorted list: ", Smart_Cart)
+
+
+
