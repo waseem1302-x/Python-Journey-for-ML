4fbe1d8b63ebef304e4716e8c9db8c8b5a0bb0b2
Waseem Mushtaq <waseemmushtaq045@gmail.com>
2025-07-29 12:49:55 +0800

    🧠 Practiced tuples – added 2_tuple.py

---
+++ 03_DataStructures/2_tuple.py
@@ -0,0 +1,23 @@
+#tuple
+
+tup = (1,2,3,4,5)
+print(tup)
+print(type(tup))
+
+#accessing tuble el
+
+print(tup[0])
+print(tup[-1])
+print(tup[0:2])
+
+
+# Tuple Operations
+
+print(len(tup))    #length
+print(5 in tup)    #Membership Check
+
+# Nested_tuple
+
+nested = (1,2, (3,4))
+print(nested)
+print(nested[1:3])
