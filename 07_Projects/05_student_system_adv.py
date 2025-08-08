3b542f0a53424755031fc2f1efb4a5bbf8ccaa02
Waseem Mushtaq <waseemmushtaq045@gmail.com>
2025-08-08 19:36:23 +0800

    ✨ Started Student Management System project - class and function skeleton added

---
+++ 07_Projects/05_student_system_adv.py
@@ -0,0 +1,29 @@
+class Student:
+
+    def __init__(self, name, age, ID, courses, gpa):
+        self.name = name
+        self.age = age
+        self.ID = ID
+        self.courses = courses
+        self.gpa = gpa
+        
+        
+    def register(self):
+        return
+    
+students = []
+
+def add_student():
+    return
+
+def view_all_std():
+    return
+
+def delete():
+    return
+
+def search():
+    return
+
+def sort():
+    return
